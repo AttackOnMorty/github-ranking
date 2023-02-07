9508f9978dae237c8586f84b536d245f8ce5d585
Luke Mao [SSW] <ErenJagerM@outlook.com>
2023-02-07 21:03:37 +0800

    style: lint and format

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,17 @@
-import { ReportHandler } from 'web-vitals';
+import { type ReportHandler } from 'web-vitals';
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+  if (onPerfEntry != null && onPerfEntry instanceof Function) {
+    void import('web-vitals').then(
+      ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      }
+    );
   }
 };
 
