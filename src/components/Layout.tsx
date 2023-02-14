a491f049e4b21dab76f8ddcf2f9b0b89c4bfa00c
Luke Mao [SSW] <ErenJagerM@outlook.com>
2023-02-14 17:33:51 +0800

    feat: no underline on name link

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -62,7 +62,7 @@ const Layout: React.FC = () => {
           <span className="text-sm font-light">
             Crafted with <HeartFilled style={{ color: '#eb2f96' }} /> by{' '}
             <a
-              className="text-blue-500 hover:underline"
+              className="text-blue-500 hover:underline no-underline"
               href="https://github.com/AttackOnMorty"
               target="_black"
               rel="noreferrer"
