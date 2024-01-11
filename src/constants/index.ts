c6d42786135df6a6232a022527669d1e7de9b4bd
AttackOnMorty <erenjagerm@outloook.com>
2024-01-11 10:41:43 +1000

    Update MAX_DATA_COUNT constant to 1000

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 export const PAGE_SIZE = 20;
 
-export const MAX_DATA_COUNT = 100;
+// NOTE: 1000 is the max count of data that can be exported. See https://docs.github.com/en/rest/search/search?apiVersion=2022-11-28#about-search
+export const MAX_DATA_COUNT = 1000;
 
 export const EMPTY = '-';
 
