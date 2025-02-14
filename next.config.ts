6a0f38a51851d4c0eb4d0cdbc1fdd1f1b43c55b6
Luke Mao [SSW] <ErenJagerM@outlook.com>
2025-02-14 10:33:57 +1000

    🌐 Configure image optimization to be unoptimized in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
+    // https://vercel.com/docs/image-optimization/managing-image-optimization-costs#minimizing-image-optimization-costs
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
